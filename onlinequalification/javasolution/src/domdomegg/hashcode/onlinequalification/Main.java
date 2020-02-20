f871110c62bcc9f4a3fe8920c1748874d5779d68
Adam Jones <jones_adam@rocketmail.com>
2020-02-20 18:11:36 +0000

    Parse input in Java

--- onlinequalification/javasolution/src/domdomegg/hashcode/onlinequalification/Main.java
+++ onlinequalification/javasolution/src/domdomegg/hashcode/onlinequalification/Main.java
@@ -4,6 +4,11 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.nio.charset.StandardCharsets;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
 public class Main {
 
@@ -11,8 +16,74 @@ public class Main {
         InputStreamReader reader = new InputStreamReader(System.in, StandardCharsets.UTF_8);
         BufferedReader in = new BufferedReader(reader);
         String line;
+
+        line = in.readLine();
+        int B = Integer.parseInt(line.split(" ")[0]);
+        int L = Integer.parseInt(line.split(" ")[1]);
+        int D = Integer.parseInt(line.split(" ")[2]);
+
+        List<Book> books = Arrays.stream(in.readLine().split(" "))
+                .map(Integer::parseInt)
+                .map(Book::new)
+                .collect(Collectors.toList());
+        assert books.size() == B;
+
+        List<Library> libraries = parseLibraries(in, books);
+
+        System.out.println("B = " + B);
+        System.out.println("L = " + L);
+        System.out.println("D = " + D);
+        System.out.println("books = " + books);
+        System.out.println("libraries = " + libraries);
+    }
+
+    private static List<Library> parseLibraries(BufferedReader in, List<Book> allBooks) throws IOException {
+        List<Library> libraries = new ArrayList<>();
+        String line;
         while ((line = in.readLine()) != null) {
-            System.out.println(line);
+            int signupProcessLength = Integer.parseInt(line.split(" ")[1]);
+            int shippingSpeed = Integer.parseInt(line.split(" ")[2]);
+            line = in.readLine();
+            List<Book> books = Arrays.stream(line.split(" ")).map(Integer::parseInt).map(allBooks::get).collect(Collectors.toList());
+            libraries.add(new Library(signupProcessLength, shippingSpeed, books));
+        }
+
+        return libraries;
+    }
+
+    private static class Library {
+        int signupProcessLength;
+        int shippingSpeed;
+        List<Book> books;
+
+        public Library(int signupProcessLength, int shippingSpeed, List<Book> books) {
+            this.signupProcessLength = signupProcessLength;
+            this.shippingSpeed = shippingSpeed;
+            this.books = books;
+        }
+
+        @Override
+        public String toString() {
+            return "Library{" +
+                    "signupProcessLength=" + signupProcessLength +
+                    ", shippingSpeed=" + shippingSpeed +
+                    ", books=" + books +
+                    '}';
+        }
+    }
+
+    private static class Book {
+        int score;
+
+        public Book(int score) {
+            this.score = score;
+        }
+
+        @Override
+        public String toString() {
+            return "Book{" +
+                    "score=" + score +
+                    '}';
         }
     }
 }
